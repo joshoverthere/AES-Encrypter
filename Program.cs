79d355a892426c41f736d812510e1c4e3120c480
joshoverthere <contact@xeta.space>
2022-01-31 14:52:10 +0000

    breakBlocks function working.

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@ namespace AES
     class Program
     {
         
-        static string[,] generateState(string message)
+        static List<List<string>> generateState(List<string> block)
         {
-            string[,] state = new string[4,4];
+            List<List<string>> state = new List<List<string>>();    
             return state;
         }
 
-        static List<string[]> breakBlocks(List<string> bytes)
+        //This function breaks a list of bytes into lists (blocks) of 16 bytes each (adding empty bytes so that all blocks are full)
+        static List<List<string>> breakBlocks(List<string> bytes)
         {
-            List<string[]> blocks = new List<string[]>();
+            List<List<string>> blocks = new List<List<string>>();
 
+            //add empty bytes if the number of bytes is not divisible by 16
             if (bytes.Count % 16 != 0)
             {
                 Console.WriteLine("Adding empty bytes...");
@@ -26,10 +28,22 @@ namespace AES
                 }
             }
 
+            //create blocks of 16 bytes each and add them to the list of blocks
+            while (bytes.Count > 0)
+            {
+                List<string> block = new List<string>();
+                for (int i = 0; i < 16; i++)
+                {
+                    block.Add(bytes[0]);
+                    bytes.RemoveAt(0);
+                }
+                blocks.Add(block);
+            }
+
+            Console.WriteLine("Number of blocks : " + blocks.Count.ToString());
             
 
             return blocks;
-            //This function breaks a 1D array of bytes into blocks of 16 bytes each (adding empty bytes so that all blocks are full)
 
         }
         
@@ -57,7 +71,7 @@ namespace AES
                     bytes.Add(Convert.ToString(element, 2));
                 }
 
-                List<string[]> blocks = breakBlocks(bytes);
+                List<List<string>> blocks = breakBlocks(bytes);
 
                 
 
